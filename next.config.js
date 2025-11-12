d2acf49c2efa2f426659af08a10e99812c5663bd
Aditya Kumar <adityakiitdelhi@gmail.com>
2025-11-12 16:11:51 +0530

    uopdates

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/flappy/" : "",
+  assetPrefix: isProd ? "/flappy-gulla/" : "",
   images: {
     unoptimized: true,
   },
