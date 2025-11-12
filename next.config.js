9316bc80dc282f7cb6fb60de1d9717a3204c13ff
Aditya Kumar <adityakiitdelhi@gmail.com>
2025-11-12 17:57:45 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProd ? "/flappy-gulla/" : "",
+  basePath: isProd ? "/flappy-gulla" : "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  output: 'export',
 };
